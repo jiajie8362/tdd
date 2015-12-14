47b642633bf387d8764c6d6417e5c4dea5b543bc
jet <jiajie8362@gmail.com>
2015-12-15 00:01:39 +0800

    Front page HTML now generated from a template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -15,18 +15,19 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
-        head_text = self.browser.find_element_by_tag_name('h1').text
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do',header_text)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(inputbox.get-attribute('placeholder'), 'Enter a to-do item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         inputbox.send_keys('But peacock feathers')
         inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
+        "New to-do item did not appear in table")
 
         self.fail('Finish the test!')
 if __name__ == '__main__':
