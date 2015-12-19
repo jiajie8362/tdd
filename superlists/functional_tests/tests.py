21e8b1daa0465b5eb8672b4a2ceae5e00c411c66
jet <jiajie8362@gmail.com>
2015-12-19 22:59:05 +0800

    [JJ]

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+        
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
 
@@ -27,6 +32,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         edith_list_url = self.browser.current_url
+
         self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
@@ -34,8 +40,8 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly') 
-        self.check_for_row_in_list_table('1: Buy peacock feathers') 
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         self.browser.quit()
         self.browser = webdriver.Firefox()
